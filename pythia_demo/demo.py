be141f91f230431b8b09ba624ecffc26cbb58218
Pedro Pablo Correa <pbcorrea@uc.cl>
2021-04-26 14:00:17 -0400

    ADD: Register events using VideoRecorder

--- pythia_demo/demo.py
+++ pythia_demo/demo.py
@@ -2,33 +2,37 @@
 
 import json
 
+import pyds
+
 from pythiags import frames_per_batch, objects_per_frame, Producer, Consumer
 from pythiags.deepstream.iterators import analytics_per_frame
 from pythiags.deepstream.iterators import object_analytics_per_frame
 from pythiags.deepstream.iterators import analytics_per_object
 from pythiags.deepstream.parsers import detector_bbox
 
-import pyds
-
-from dump import dump_metadata
 from models import Detection
 from common import Session
+from dump import register_detection
+from dump import register_frame
+
 
 class MyCustomExtract(Producer):
     def extract_metadata(self, pad, info):
         meta = {}
         for frame_metadata in frames_per_batch(info):
-            key = (frame_metadata.source_id,frame_metadata.frame_num)            
+            key = (frame_metadata.source_id, frame_metadata.frame_num)
             filtered_frame_analytics = []
             for frame_analytics in analytics_per_frame(frame_metadata):
-                filtered_frame_analytics.append({
-                    'objCnt': frame_analytics.objCnt,
-                    'objInROIcnt': frame_analytics.objInROIcnt,
-                    'objLCCumCnt': frame_analytics.objLCCumCnt,
-                    'objLCCurrCnt': frame_analytics.objLCCurrCnt,
-                    'ocStatus': frame_analytics.ocStatus,
-                    'unique_id': frame_analytics.unique_id, 
-                })
+                filtered_frame_analytics.append(
+                    {
+                        "objCnt": frame_analytics.objCnt,
+                        "objInROIcnt": frame_analytics.objInROIcnt,
+                        "objLCCumCnt": frame_analytics.objLCCumCnt,
+                        "objLCCurrCnt": frame_analytics.objLCCurrCnt,
+                        "ocStatus": frame_analytics.ocStatus,
+                        "unique_id": frame_analytics.unique_id,
+                    }
+                )
             detection_count = 0
             detection_metadata = []
             for obj_meta in objects_per_frame(frame_metadata):
@@ -38,34 +42,73 @@ class MyCustomExtract(Producer):
                 for obj_analytics in analytics_per_object(obj_meta):
                     object_count += 1
                     object_analytics = {
-                        'lcStatus': obj_analytics.lcStatus,
-                        'dirStatus': obj_analytics.dirStatus,
-                        'ocStatus': obj_analytics.ocStatus,
-                        'roiStatus': obj_analytics.roiStatus,
+                        "lcStatus": obj_analytics.lcStatus,
+                        "dirStatus": obj_analytics.dirStatus,
+                        "ocStatus": obj_analytics.ocStatus,
+                        "roiStatus": obj_analytics.roiStatus,
                         "source_id": frame_metadata.source_id,
                         "frame_num": frame_metadata.frame_num,
                     }
                     object_data.append(object_analytics)
                 detection_count += 1
                 detection = {
-                "label" : obj_meta.obj_label,
-                "confidence": obj_meta.confidence,
-                "frame_number": frame_metadata.frame_num,
-                "bbox": detector_bbox(obj_meta), 
-                "objects": object_data
+                    "tracked_object_id": obj_meta.object_id,
+                    "label": obj_meta.obj_label,
+                    "confidence": obj_meta.confidence,
+                    "frame_number": frame_metadata.frame_num,
+                    "bbox": detector_bbox(obj_meta),
+                    "objects": object_data,
                 }
                 detection_metadata.append(detection)
-                if len(object_data)>1:
-                    import pdb;pdb.set_trace()
             meta[key] = {
-                'analytics':filtered_frame_analytics, 
-                'detections':detection_metadata
-                }
+                "analytics": filtered_frame_analytics,
+                "detections": detection_metadata,
+            }
         return meta
 
 
-class MyCustomProcess(Consumer):
+class DDBBWriterProcess(Consumer):
+    def __init__():
+        self.video_recorder = VideoRecorder(*a, **kw)
+        self.selected_rois = SELECTED_ROIS
+        self.Session = Session
+
+    def dump_metadata(meta: dict, db_session):
+        for (source_id, frame_number), full_metadata in meta.items():
+            frame_metadata = full_metadata["analytics"]
+            detection_metadata = full_metadata["detections"]
+
+            important_detections = self.filter_detections(detection_metadata)
+
+            if not len(important_detections):
+                return
+
+            # Check if event has already been registered
+            event_id = register_event(detections)
+
+            registered_detections = []
+            for detection in important_detections:
+                rois = []
+                for obj in detection["objects"]:
+                    rois.extend(obj["roiStatus"])
+                registered_detection = register_detection(detection, rois, event_id)
+                registered_detections.append(registered_detection)
+
+            registered_frame = register_frame(
+                frame_metadata, source_id, frame_number, registered_detections
+            )
+            db_session.add(registered_frame)
+        db_session.commit()
+
+    def filter_detections(self, detections: list) -> list:
+        important_detections = []
+        for detection in detections:
+            if not check_detection_importance(detection, self.selected_rois):
+                continue
+            important_detections.append(detection)
+        return important_detections
+
     def incoming(self, events):
         # This can be as slow as required
-        db_session = Session()
-        dump_metadata(events, db_session)
+        session = self.Session()
+        dump_metadata(events, session)
