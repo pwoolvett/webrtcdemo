3bff7cfd3f0237e6878449a13a74705719fbbfea
Pedro Pablo Correa <pbcorrea@uc.cl>
2021-04-26 17:59:42 -0400

    FMT: Minor fix

--- pythia_demo/common.py
+++ pythia_demo/common.py
@@ -1,7 +1,6 @@
 from models import get_session
 from models import get_engine
 from models import create_database
-import sqlalchemy
 
 db_connection_string = "sqlite:////db/test.db"
 Session = get_session(db_connection_string)
