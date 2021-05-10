83d69cf97449ea3786d86df3a2bee084f00fc802
Pedro Pablo Correa <pbcorrea@uc.cl>
2021-05-10 11:51:55 -0400

    nomod

--- frontend_flask/app/forms.py
+++ frontend_flask/app/forms.py
@@ -1,12 +1,16 @@
 from datetime import date
+from datetime import datetime
 from datetime import time
 
 from flask_wtf import FlaskForm
 from wtforms import SelectField
+from wtforms import RadioField
 from wtforms.validators import DataRequired
 from wtforms.fields.html5 import DateField
 from wtforms.fields.html5 import TimeField
+
 from wtforms import SubmitField
+
 
 from app import app
 
@@ -18,17 +22,21 @@ class TimeFilterForm(FlaskForm):
         "Start Date", default=date(2021, 1, 1), validators=[DataRequired()]
     )
     date_to = DateField(
-        "End Date", default=date(2021, 12, 31), validators=[DataRequired()]
+        "End Date",
+        default=lambda: datetime.now().date(),
+        validators=[DataRequired()]
     )
     time_from = TimeField(
         "Start Time", default=time(0, 0, 0), validators=[DataRequired()]
     )
     time_to = TimeField(
-        "End Time", default=time(23, 59, 59), validators=[DataRequired()]
+        "End Time",
+        default=lambda: datetime.now().time(),
+        validators=[DataRequired()]
     )
     camera_id = SelectField(
         "Camera ID",
-        choices=[(key, value) for key, value in enumerate(AVAILABLE_CAMERAS)],
+        choices=[(key, value) for key, value in enumerate(AVAILABLE_CAMERAS)],  # TODO: update when available cameras is a mapping
         validators=[DataRequired()],
     )
-    submit = SubmitField("View results")
+    # submit = SubmitField("View results") NOTE: implemented in template
